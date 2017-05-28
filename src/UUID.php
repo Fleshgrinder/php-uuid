bc31d4bef16e27eb8a3160e8112e978bb71a3675
Richard Fussenegger <fleshgrinder@users.noreply.github.com>
2017-05-28 16:46:14 +0200

    Numbers are easier to read

--- src/UUID.php
+++ src/UUID.php
@@ -274,10 +274,10 @@ if (class_exists('UUID') === false) {
 		 *
 		 * The following UUID representations are parsable:
 		 *
-		 * - hexadecimal representations (`aaaaaaaabbbbccccddddeeeeeeeeeeeee`),
-		 * - string representations (`aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeeee`),
-		 * - URNs (`urn:uuid:aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeeee`), and
-		 * - Microsoft representations (`{aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeeee}`).
+		 * - hexadecimal (`00000000111122223333444444444444`),
+		 * - string (`00000000-1111-2222-3333-444444444444`),
+		 * - URNs (`urn:uuid:00000000-1111-2222-3333-444444444444`), and
+		 * - Microsoft (`{00000000-1111-2222-3333-444444444444}`).
 		 *
 		 * Leading and trailing whitespace, namely spaces (` `) and tabs (`\t`), is
 		 * ignored, so are leading opening braces (`{`) and trailing closing braces
